cb4a69ba5795cd1b769c3699d828bd4ab2d5a94b
Brylle <catada04@gmail.com>
2025-09-15 15:47:59 +0800

    try paging

--- app/controllers/CrudController.php
+++ app/controllers/CrudController.php
@@ -20,19 +20,37 @@ class CrudController extends Controller {
           $this->call->view('Pet', $data);
     }
 
-    function show1(){
-        $this->call->database();
-        if($this->db){
-            echo 'sige';
-        }else{
-            echo 'wag';
-        }
-    }
 
-    function show(){      
-        $data ['user'] = $this->PetModel->all();
+
+    function show() {             
+        $page = 1;
+        if(isset($_GET['page']) && ! empty($_GET['page'])) {
+            $page = $this->io->get('page');
+        }
+
+        $q = '';
+        if(isset($_GET['q']) && ! empty($_GET['q'])) {
+            $q = trim($this->io->get('q'));
+        }
+
+        $records_per_page = 10;
+
+        $all = $this->author_model->page($q, $records_per_page, $page);
+        $data['all'] = $all['records'];
+        $total_rows = $all['total_rows'];
+        $this->pagination->set_options([
+            'first_link'     => '⏮ First',
+            'last_link'      => 'Last ⏭',
+            'next_link'      => 'Next →',
+            'prev_link'      => '← Prev',
+            'page_delimiter' => '&page='
+        ]);
+        $this->pagination->set_theme('custom'); // or 'tailwind', or 'custom'
+        $this->pagination->initialize($total_rows, $records_per_page, $page, site_url('user').'?q='.$q);
+        $data['user'] = $this->pagination->paginate();
         $this->call->view('data', $data);
     }
+    
 
     public function create(){
         if($this->io->method() =='post'){
--- app/views/data.php
+++ app/views/data.php
@@ -7,6 +7,17 @@
     <link rel="stylesheet" href="<?=base_url();?>public/css/data.css">
 </head>
 <body>
+    <div>
+        <form action="<?=site_url('user');?>" method="get" class="col-sm-4 float-end d-flex">
+		<?php
+		$q = '';
+		if(isset($_GET['q'])) {
+			$q = $_GET['q'];
+		}
+		?>
+        <input class="form-control me-2" name="q" type="text" placeholder="Search" value="<?=html_escape($q);?>">
+        <button type="submit" class="btn btn-primary" type="button">Search</button>
+	</form>
     <h1>Welcome to Pet's Data</h1>
     <table border="1">
         <tr>
@@ -30,6 +41,8 @@
         <?php endforeach;?>    
     </table>
             <a href="<?=site_url('/pet/create');?>">Create</a>
-
+    <?php
+	echo $page;?>
+      </div>
 </body>
 </html>
