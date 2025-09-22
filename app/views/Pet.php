4247d3e7e6befc7f0d179cea3bdede7d59490055
Brylle <catada04@gmail.com>
2025-09-22 18:55:18 +0800

    ganunpala

--- app/views/Pet.php
+++ app/views/Pet.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet</title>
-    <link rel="stylesheet" href="<?=base_url();?>public/css/pet.css">
+    <link rel="stylesheet" href="<?=base_url();?>public/css/home.css">
 </head>
 <body>
     <!-- Hero -->
